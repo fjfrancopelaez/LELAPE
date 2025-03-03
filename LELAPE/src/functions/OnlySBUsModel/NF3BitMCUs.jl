a2e20a9b16652b9d481e874ee7a8711f242387fe
fjfrancopelaez <fjfranco@fis.ucm.es>
2025-03-03 17:26:24 +0100

    Issue #12b: NF3BitMCUs admits different reading rounds

--- LELAPE/src/functions/OnlySBUsModel/NF3BitMCUs.jl
+++ LELAPE/src/functions/OnlySBUsModel/NF3BitMCUs.jl
@@ -90,3 +90,100 @@ function NF3BitMCUs(NSBU::Int, NMU2::Int, LN::Int, METHOD::String, D::Int, WordW
     return NFM3A_OPT+NFM3B_OPT, NFM3A_PES+NFM3B_PES
 
 end
+
+function NF3BitMCUs(SBU_VECTOR::Vector{Int}, MU2_VECTOR::Vector{Int}, LN::Int, METHOD::String, D::Int, WordWidth::Int=1, UsePseudoAddress::Bool=true)::Tuple{Float64, Float64}
+
+    # indicates the expected number of false 3-bit MCUs that will occur 
+    # in a memory with LN bits in which several reading cycles were done. In every round there 
+    # were observed NSBU single bit upsets and NMU2 double bit upsets. NSBU and NMU2 are used 
+    # to build a vector of the very same size. . 
+    # In this analysis, MCUS are sought using some grouping --method (METHOD) with a generalized 
+    # distance D.HD
+    #
+    # Admitted values for METHOD and D are the following:
+    #   
+    #   1) METHOD: "MBU" --> Only MBUs are sought. In this case, D is the Wordwidth.
+    #   2) METHOD: "MHD" --> Only possible if the user has been able to place the ExtractFlippedBits
+    #                        cell in the XY plane. Two cells are related if |x1-x2|+|y1-y2| <= D. This 
+    #                        is the Manhattan distance.
+    #   3) METHOD: "IND" --> Only possible if the user has been able to place the ExtractFlippedBits
+    #                        cell in the XY plane. Two cells are related if max(|x1-x2|,|y1-y2|) <= D.
+    #                        In mathematics, this is the "infinite distance".
+    #   4) METHOD: "THD" --> Only valid if pairs of bitflips are located in a linear bitstream and if the
+    #                        distace between cells is smaller than D: |x1-x2| <= D.
+    #   5) METHOD: "XOR" --> Related pairs are got by means of statistical deviations. Addresses are XORed 
+    #                        and only if the value is one of the D possible critical values. If the WORD Addresses  
+    #                        is used instead of PSEUDOADDRESS, the memory size must be expressed in WORDs, 
+    #                        LA = LN/WordWidth.
+    #                        IF SO, THE WORDWIDTH MUST BE PROVIDED.
+    #   6) METHOD: "POS" --> Identical to the previous one but with positive subtraction instead of XOR.
+    #   
+    #   Everything can be found in Eq. 11 of F. J. Franco, J. A. Clemente, G. Korkian, 
+    #   J. C. Fabero, H. Mecha and R. Velazco, "Inherent Uncertainty in the Determination of Multiple 
+    #   Event Cross Sections in Radiation Tests," IEEE Transactions on Nuclear Science, vol. 67, no. 7, 
+    #   pp. 1547-1554, July 2020, doi: 10.1109/TNS.2020.2977698.
+    #
+    #   In this paper, it was demonstrated that it is mathematically impossible to get an exact value. Therefore, 
+    #   optimistic and pessimistic results are provided.
+    #
+    #   First, the memory size if corrected in case WORD ADDRESS is used in XOR or POS.  
+    #
+
+    ((UsePseudoAddress==false) && (METHOD in ["XOR", "POS"])) ? L = div(LN,WordWidth) : L = LN
+    
+    # Now, the size of the SBU influence area and the 2-bit ones.
+    if METHOD == "MBU" 
+        S1 = (D-1)
+        S2S = S1
+        S2L = S1
+    elseif METHOD == "MHD"
+        S1 = 2*D*(D+1)
+        S2S = 2D^2 + 4D
+        S2L = 2D^2 + 5D -1 + 2*(D-1)*div(D,2) + 2*div(D+1,2)*(div(D+1,2)-1)
+    elseif METHOD == "IND"
+        S1 = 4*D*(D+1)
+        S2S = 4D^2 + 6D
+        S2L = 7D^2 + 6D - 1
+    elseif METHOD == "THD"
+        S1 = 2*(D-1)
+        S2S = 2D-2
+        S2L = 3D-4
+    elseif METHOD == "XOR"
+        S1 = D
+        S2S = D-1
+        S2L = 2D-2
+    elseif METHOD == "POS"
+        S1 = 2D
+        S2S = 2D - 1
+        S2L = 4D - 2
+    else
+        error("Unknown operation!!!!")
+    end
+
+    if (length(SBU_VECTOR)!=length(MU2_VECTOR))
+        error("Set size mismatch!!")
+    end
+    if (sum(SBU_VECTOR.<0)!=0)
+        error("Negative elements in the SBU vector")
+    end
+    if (sum(MU2_VECTOR.<0)!=0)
+        error("Negative elements in the NMU2 vector")
+    end
+
+    # Unlike in the previous version of the function the NT and NSBU*NMU2 are treated
+    # as vectors.
+    NT =  sum(NTriplets.(SBU_VECTOR))
+
+    NFM3A_OPT = NT*S1*(S1-1)/L^2
+    NFM3A_PES = 3*NFM3A_OPT
+
+    # Number of false 3-bit MCUs related to 2-bit MCU + 1 SBU.
+    # First, the influence areas of 2-bit MCUs.
+
+    NFM3B_OPT = sum(SBU_VECTOR.*MU2_VECTOR)*S2S/L
+    NFM3B_PES = NFM3B_OPT/S2S*S2L
+
+    return NFM3A_OPT+NFM3B_OPT, NFM3A_PES+NFM3B_PES
+
+
+end
